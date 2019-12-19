e88c99c9088fb9176d47d0e48501d489e68a7be8
peproneh <peproneh@gmail.com>
2019-12-19 23:34:56 +0400

    Converted to the use functions

--- SentenceCounter.py
+++ SentenceCounter.py
@@ -1,15 +1,37 @@
+#this function counts ". "s of the string
+def dotSpaceCounter(str):
+    n=str.count(". ")
+    return (n)
+#this function counts ". enter"s of the string
+def dotEnterCounter(str):
+    m=str.count(".\n")
+    return (m)
+
+# this is a function that counts the number of sentences
+def sentenceCounter(s):
+    # number of sentences
+    number = dotSpaceCounter(s)
+    number = number + dotEnterCounter(s)
+    # checking whether the last sentence is counted
+    if str.endswith('.'):
+        return (number+1)
+    else:
+        return(number)
+
 # This is a program, which counts the number of sentences in the story.txt file
-#opening text file
-f=open("story.txt", "r")
-#reading text from the file
-str=f.read()
-#counting sentences of the string
-num=str.count(". ")
-num=num+str.count(".\n")
-#checking whether the last sentence is counted
-if str.endswith('.') : num=num+1
-else: num=num
-#printing counted sentence number
+# opening text file
+f = open("story.txt", "r")
+# reading text from the file
+str = f.read()
+# calling for the sentenceCounter method
+num = sentenceCounter(str)
+# printing counted sentence number
 print(num)
+f.close()
+
+
+
+
+
 
 
